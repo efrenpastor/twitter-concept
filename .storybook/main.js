1c973dc529ae0ea6a01385336915a4b94c5f3ae9
efren <efren@holavilma.com>
2022-08-09 10:36:58 +0200

    chore: add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "stories": [
+    // "../components/**/*.stories.@(js|jsx)",
+    "../stories/**/*.stories.@(js|jsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "storybook-addon-next"
+  ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-webpack5"
+  }
+}
