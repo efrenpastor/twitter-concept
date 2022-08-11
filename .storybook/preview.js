cdcf2b8f1e0b6ff2ab46e7bf104f0d45b8e9df48
efren <efren@holavilma.com>
2022-08-11 11:26:47 +0200

    feature: Supabase config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,14 @@
+import { addDecorator } from '@storybook/react'
+import { UserProvider } from '@supabase/auth-helpers-react'
+import { supabaseClient } from '@supabase/auth-helpers-nextjs'
+
 import '../styles/globals.css'
+
+addDecorator((storyFn) => (
+  <UserProvider supabaseClient={supabaseClient}>
+    {storyFn()}
+  </UserProvider>
+))
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
+import { UserProvider } from '@supabase/auth-helpers-react'
+import { supabaseClient } from '@supabase/auth-helpers-nextjs'
 
-function MyApp ({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App ({ Component, pageProps }) {
+  return (
+    <UserProvider supabaseClient={supabaseClient}>
+      <Component {...pageProps} />
+    </UserProvider>
+  )
 }
-
-export default MyApp
